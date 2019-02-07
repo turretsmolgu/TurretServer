0d729a3a19d76cd9e4c3b9d9ec75e04c84ccf528
knpreacher <knpreacher@gmail.com>
2019-02-07 15:43:59 +0300

    'init'

---
+++ TurretServerApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TurretserverappConfig(AppConfig):
+    name = 'TurretServerApp'
---
+++ TurretServerApp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+from TurretServerApp import views
+
+urlpatterns = [
+    #path('<str:par>/', views.index),
+    path('', views.index, name='index')
+]
+
